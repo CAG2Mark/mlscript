4b6b344e6116bb5b24e9f8bcf17304e5e756a55c
CAG2Mark <git@markng.com>
2026-08-25 14:18:51 +0800

    Revert "WIP stack safety"
    This reverts commit b938cf1ccd1c4e26569f2e74b2fef03433c52743.

--- hkmc2/shared/src/test/mlscript-compile/Runtime.mjs
+++ hkmc2/shared/src/test/mlscript-compile/Runtime.mjs
@@ -1058,9 +1058,6 @@ lambda7 = (undefined, function (Runtime2, EffectHandle1, value) {
   }
   static validateContTrace(tr, loc) {
     let remainIter, realLastHandler, realLast, scrut, scrut1, scrut2;
-    if (false === true) {
-      return runtime.Unit
-    }
     remainIter = 100000;
     realLastHandler = tr;
     lbl: while (true) {
